50315ed849e9638b0ff42c4679f67e0bb93cd8b6
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2026-03-25 19:21:15 -0700

    fix: hyperlink entire runtime

--- apps/www/components/templates/table-cells/runtime.tsx
+++ apps/www/components/templates/table-cells/runtime.tsx
@@ -20,36 +20,53 @@ const RuntimeTableCell: React.FC<RuntimeTableCellProps> & { Skeleton: React.FC }
   className,
   runtimeName,
 }) => {
+  let url;
+  let Icon;
+  if (runtimeName === 'llama.cpp') {
+    url = 'https://github.com/ggerganov/llama.cpp';
+    Icon = LogoImg.Ggml;
+  } else if (runtimeName === 'mlx_lm') {
+    url = 'https://github.com/ml-explore/mlx-lm';
+    Icon = LogoImg.Mlx;
+  }
+
+  if (url && Icon) {
+    return (
+      <a
+        className={twMerge(
+          clsx(
+            'group/runtime-table-cell flex w-fit items-center gap-1.5 hover:underline focus-visible:rounded',
+            className,
+          ),
+        )}
+        href={url}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <span
+          className="flex size-4 items-center justify-center rounded"
+          runtime-table-cell-icon=""
+        >
+          <Icon
+            className="border-gray-7 transition-colors group-hover/runtime-table-cell:border-gray-8"
+            size={16}
+          />
+        </span>
+        <span className="line-clamp-1 leading-4" runtime-table-cell-name="">
+          {runtimeName}
+        </span>
+      </a>
+    );
+  }
+
   return (
     <div className={twMerge(clsx('flex items-center gap-1.5', className))}>
-      {runtimeName === 'llama.cpp' ? (
-        <a
-          className="focus-visible:rounded"
-          href="https://github.com/ggerganov/llama.cpp"
-          target="_blank"
-          rel="noreferrer"
-          runtime-table-cell-icon=""
-        >
-          <LogoImg.Ggml className="border-gray-7 transition-colors hover:border-gray-8" size={16} />
-        </a>
-      ) : runtimeName === 'mlx_lm' ? (
-        <a
-          className="focus-visible:rounded"
-          href="https://github.com/ml-explore/mlx-lm"
-          target="_blank"
-          rel="noreferrer"
-          runtime-table-cell-icon=""
-        >
-          <LogoImg.Mlx className="border-gray-7 transition-colors hover:border-gray-8" size={16} />
-        </a>
-      ) : (
-        <span
-          className="flex size-4 items-center justify-center rounded border border-gray-6 bg-gray-5 text-gray-11"
-          runtime-table-cell-icon=""
-        >
-          <FileQuestionMark className="size-2.5" />
-        </span>
-      )}
+      <span
+        className="flex size-4 items-center justify-center rounded border border-gray-6 bg-gray-5 text-gray-11"
+        runtime-table-cell-icon=""
+      >
+        <FileQuestionMark className="size-2.5" />
+      </span>
       <span className="line-clamp-1 leading-4" runtime-table-cell-name="">
         {runtimeName}
       </span>
